de71b8f1773d1f54d6ff6890ef66b6e397a03cfc
Nicolas Hamelin <n.hamelin44120@gmail.com>
2022-03-31 15:29:06 +0200

    first test

---
+++ C#/Trivia/Tests/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace MachineACafe.Test
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            var test1 = 1;
+            var test2 = 1;
+
+            Assert.Equal(test1, test2);
+        }
+    }
+}
